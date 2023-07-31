4aada89aa065d6f5a83b894b3852e8dd0e8f8329
ArifulIslam Shuvo <arifulislams063@gmail.com>
2023-07-31 22:34:59 +0600

    PC-builder-backend initialize

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+const cors = require("cors");
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xadal2t.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const PcBuilderProductsCollection = client.db("PC-Builder").collection("products");
+    console.log(" connect successfully! ");
+
+    
+   
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Pc-builder!");
+});
+
+app.listen(port, () => {
+  console.log(`Server Running app listening on port ${port}`);
+});
